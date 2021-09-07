622fb63bd321ff9257f4f4bfa622c495abb98253
williamgrimes <williamgrimes@gmx.com>
2021-09-07 14:04:11 +0200

    Add symlink for .gitconfig

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p ~/.config/fish/
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
