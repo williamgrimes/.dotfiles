7e3bb6f6c46008c9fccfa33d66b0cb8b1d08d5f9
williamgrimes <williamgrimes@gmx.com>
2022-02-24 15:43:03 +0100

    Update symlink_setup.sh

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -4,4 +4,4 @@ ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/personal/ ~/.emacs.d/personal/
+ln -s ~/.dotfiles/personal ~/.emacs.d/
